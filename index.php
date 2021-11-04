ce0d2259877ad464071ea094cf743d8a2cf927c9
dunnticia633 <dunnticia633@gmail.com>
2021-11-04 15:14:36 -0400

    finished READ function

--- index.php
+++ index.php
@@ -60,13 +60,64 @@
         mysqli_close($conn); 
     };
 
-    // // Read
-    // function readAllHeroes($name) {
-    //     // output heroes from array
-    // };
+    // Read
+    function readAllHeroes() {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "mySuperheroes";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $SQL = "SELECT * FROM heroes";
+        $result = $conn->query($SQL);
+
+        if ($result->num_rows > 0) {
+        // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "Name: " . $row["name"]. " " . $row["about_me"]. " " . $row["biography"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+    }
 
     // // Update
-    // function updateHero($name, $tagline) {};
+    // function updateHero($id, $name, $tagline) {
+    //     echo "hello";
+    //     echo ("<h1>CREATE</h1><pre>" . $name. $tagline. $id . "</pre>");
+
+    //     $servername = "localhost";
+    //     $username = "root";
+    //     $password = "";
+    //     $dbname = "mySuperheroes";
+        
+    //     // Create connection
+    //     $conn = new mysqli($servername, $username, $password, $dbname);
+    //     // Check connection
+    //     if ($conn->connect_error) {
+    //         die("Connection failed: " . $conn->connect_error);
+    //     }
+
+    //     $SQL = "SELECT * FROM heroes";
+
+    //     $conn;
+    //     if ($conn->query($SQL) === TRUE) {
+    //         echo "New Hero created successfully";
+    //         echo '<br>';
+    //     }
+    //     else {
+    //         echo "Error: " . $SQL . "<br>" . $conn->error;
+    //     }
+
+    //     mysqli_close($conn); 
+    // };
 
     // // Delete
     // function deleteHero($name) {};
@@ -78,7 +129,7 @@
         switch($action) {
             case "create":
                 if(isset($_GET['name']) && isset($_GET['tagline']) && isset($_GET['bio'])){
-                    createHero($_GET['name'], $_GET['tagline'], $_GET['bio']);
+                    createHero($_POST['name'], $_POST['tagline'], $_POST['bio']);
                     echo "Congrats hero has name, tagline, and bio!";
                     
                 } else {
@@ -86,6 +137,7 @@
                 }
                 break;
             case "read":
+                readAllHeroes();
                 echo "display all heroes";
                 break;
             case "update":
